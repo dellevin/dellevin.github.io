0fbb065e3d31424d580ceea6513e784c666c00f5
DelLevin-Home <wonder1999@126.com>
2024-11-20 00:09:15 +0800

    Site updated: 2024-11-20 00:09:13

---
+++ js/imageList.js
@@ -0,0 +1,93 @@
+const imageList = [
+    {
+        src: "https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/08.jpg",
+        title: "济南千佛山 ⚪ 唯她",
+        imageList: [
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/08.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/01.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/07.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/02.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/03.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/04.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/05.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/refs/heads/main/%E7%9B%B8%E5%86%8C/%E6%B5%8E%E5%8D%97%E5%8D%83%E4%BD%9B%E5%B1%B1/06.jpg',
+        ],
+    },
+    {
+        src: "https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/8a638a74a6d33449519a67e9ff02a25.jpg",
+        title: "青岛之行 ⚪ 携友",
+        imageList: [
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/a30eda1535e26dd8f118931a226da87.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/9892492319ac7a846de6c84e4529b3c.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/8a638a74a6d33449519a67e9ff02a25.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/82a6e6666fa719d3955601defcb78c6.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/7e928683d6f1c0fdf1ec17a10913c5a.jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/5a74df8fe84820a9a19c81ed9f0f27c.jpg',
+        ],
+        videoList: [
+            'https://github.com/dellevin/BlogImgs/raw/main/%E7%9B%B8%E5%86%8C/%E9%9D%92%E5%B2%9B%E4%B9%8B%E8%A1%8C/86a049729588c2267449fae826db55b3.mp4'
+        ]
+    },
+    {
+        src: "https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(4).jpg",
+        title: "济南山东博物馆 ⚪ 文心",
+        imageList: [
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(3).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(2).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(1).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(4).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(5).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(6).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(7).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(8).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(9).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(10).jpg',
+            'https://raw.githubusercontent.com/dellevin/BlogImgs/main/picGoUpload/%E6%B5%8E%E5%8D%97-%E5%B1%B1%E4%B8%9C%E5%8D%9A%E7%89%A9%E9%A6%86%20(11).jpg',
+        ],
+    },
+    {
+        src: "https://user-images.githubusercontent.com/45512970/209431424-edecdf38-3363-45d1-8d9f-8d68e5618296.jpg",
+        title: "昆山京东云 ⚪ 勿念",
+        imageList: [
+            'https://user-images.githubusercontent.com/45512970/209431424-edecdf38-3363-45d1-8d9f-8d68e5618296.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431536-68c02262-1c2d-4d76-b6fb-20239156eaec.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431543-e2fc38e0-0730-4366-b52d-1d2fb41c2aa2.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431547-4d107678-541d-4bab-9a8a-bb1c915c8ab6.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431553-c507744f-9ce9-4917-a77e-7aa9472aad9a.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431559-57898948-ff67-45a4-8e41-08d7210ae177.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431561-77565989-7973-4e0d-ab39-36f088369ccb.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431563-9ddcc65b-0635-4ebe-98b8-823c7b3297b5.jpg',
+            'https://user-images.githubusercontent.com/45512970/209431567-91e71710-47c5-470d-bfba-5f21c85a43fa.jpg',
+        ],
+    },
+    {
+        src: "https://user-images.githubusercontent.com/45512970/195116669-fc6892d0-a264-4119-8e28-22de7c29154b.jpg",
+        title: "我的小米兼职 ⚪ 拾光",
+        imageList: [
+            'https://user-images.githubusercontent.com/45512970/195116293-5ca90b45-d0ee-49c4-80e3-932e1b25ffd8.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116584-fc0d43e8-5083-40dd-bb9a-ff6ee738f1d3.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116606-2ba618af-9399-4b21-98dc-fec3fa5394a3.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116650-87d2dd02-210d-42cc-8a6d-5aa7a3ecac32.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116669-fc6892d0-a264-4119-8e28-22de7c29154b.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116688-4378482d-f19c-434d-a892-828feffd1d60.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116704-014651e9-e112-46d6-af7c-0c8048310cba.jpg',
+            'https://user-images.githubusercontent.com/45512970/195116716-205015a6-471d-4e3e-b43e-6c94d2f81175.jpg',
+        ],
+    },
+    {
+        src: "https://s21.ax1x.com/2024/11/19/pAWQF3R.jpg",
+        title: "我的大学 ⚪ 流年",
+        imageList:[
+            'https://s21.ax1x.com/2024/11/19/pAWQiC9.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQF3R.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQkg1.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQAjx.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQVu6.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQZDK.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQeHO.jpg',
+            'https://s21.ax1x.com/2024/11/19/pAWQnED.jpg',
+        ]
+    }
+];
+
+export default imageList;
